4070145dfaa16430351060c8f653f9047f92a9f2
sbaute <sbaute17@gmail.com>
2026-01-08 14:54:18 -0300

    add proyect

--- 02-gift-app/src/environments/environment.ts
+++ 02-gift-app/src/environments/environment.ts
@@ -6,6 +6,7 @@ companyName2: 'App',
 companySlogan: 'Maneja tus gift',
 
 //Api keys
+// httpps://developers.giphy.com/dashboard/
 giphyApiKey: 'ZJQzpt0Ue9lBjEdkJDlqV3B1kRloFiAT',
 giphyUrl: 'https://api.giphy.com/v1',
 };
